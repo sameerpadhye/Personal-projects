a3d87fa6f1d78ab385cf8cf0d00e061b72ec0de6
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-04-30 02:43:00 +0530

    added more general information and visualization techniques for heirarchical clustering

--- data_analytic_utilities/Simple_hierarchical_clustering.R
+++ data_analytic_utilities/Simple_hierarchical_clustering.R
@@ -1,51 +1,64 @@
 ##Basic Heirarchichal clustering 
 
+# In biodiversity studies, clustering is used to visualize beta diversity patterns between different localities using species (abundance or occurrence data)
+
 #Libraries used
 library(tidyverse)
 library(readxl)
 library(ggdendro)
 library(vegan)
+library(dendextend)
 
-#importing data (if data stored in the working directory;otherwise specific path can be pasted)
+#data_file (path can be changed to where dataset is saved)
+file_clustering<-paste0(getwd(),"/data_clustering.csv")
 
-data_for_cluster<-read_excel(paste0(getwd(),"/data_clustering.xls"),
-                           sheet=1)%>%
-    dplyr::mutate_at(vars(contains('Family')),
-                     as.factor)
+#importing data     
+data_for_cluster<-read.csv(file_clustering)
 
 #Exploring the structure of data
-str(data_for_cluster)
+head(data_for_cluster,5)
 
 #For calculating the betadiversity, subset of data which is numerical is selected 
 numdata_cluster<-data_for_cluster%>%
     dplyr::select_if(is.numeric)
 
 #Converting the dataset into a distance matrix
-distance_cluster<-vegdist(t(numdata_cluster),
-                           index="jaccard", # distance index can be changed as per requirement 
-                           binary = T)# this can vary based on the type of input data (i.e. whether data is binary or not)
+distance_cluster<-vegan::vegdist(t(numdata_cluster),
+                                 index="jaccard", # distance index can be changed as per requirement 
+                                 binary = T,na.rm=T)# this can vary based on the type of input data (i.e. whether data is binary or not)
 
 #Converting the distance matrix into a 'hclust' followed by 'dendrogram' object for plotting
 object_for_plot<-hclust(distance_cluster,
                         method="average")%>% # method can be changed as per requirement
     as.dendrogram(.)
 
+#To find out the group membership of each node, cutree function with specific k value can be used; 'k' specifies the number of clusters.
+cutree(object_for_plot,k=3)
+
 #Plot using base R
-plot(object_for_plot,
-     cex=1,
-     main="Similarity between regions of Indian subcontinent",
-     ylab="Similarity",
-     xlab=NULL,
-     edgePar = list(col = c("steelblue"), 
-                    lwd = 2,
-                    lty=1),
-     horiz=F)
+plot_cluster<-plot(object_for_plot,
+                   cex=1,
+                   main="Heirarchichal clustering",
+                   ylab="Similarity",
+                   xlab=NULL,
+                   edgePar = list(col = c("steelblue"), 
+                                  lwd = 2,
+                                  lty=1),
+                   horiz=F)
+
+#plotting a triangle plot
+plot(object_for_plot,type="triangle")
 
 #Plot using ggdendro
 ggdendro::ggdendrogram(object_for_plot, 
                        rotate = TRUE, 
                        theme_dendro = TRUE,
                        size=5)+
-theme_bw(base_size=18)+
+    theme_bw(base_size=18)+
     xlab("Xlabel")+
     ylab("Name of the distance index used")
+
+# Radial plot using the package dendextend
+library(dendextend)
+ggplot(object_for_plot,horiz = T)+coord_polar(theta="x")
+
