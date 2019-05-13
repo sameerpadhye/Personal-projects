bb573e002c120e11cb2bbe0494e7a071f6864471
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-05-14 02:21:49 +0530

    Chisquare test of Independence

---
+++ data_analytic_utilities/chisq_test.R
@@ -0,0 +1,43 @@
+#Chisquare test of Independence.
+#For information on the test, please refer to https://www.statisticssolutions.com/using-chi-square-statistic-in-research/
+
+#data file path.
+
+data_file_path<-paste0(getwd(),'/data_analysis.csv')
+
+#Importing the data. 
+
+data_analysis<-read_excel(data_file_path,
+                         sheet=1)%>%
+    column_to_rownames(.,'Categories')
+
+#Exploring the dataset
+
+head(data_analysis,5)   
+
+#Visualizing the strength of association of each category with each column
+#Data is first converted into a table and then visualized by a balloonplot
+#This part has been adpoted from http://www.sthda.com/english/wiki/chi-square-test-of-independence-in-r
+
+table_data<-as.table(as.matrix(data_analysis))
+
+library(gplots)
+
+balloonplot(t(table_data), main ="Association strength", xlab ="", ylab="",
+            label = FALSE, show.margins = FALSE)
+
+#Performing the Chisquare test. 
+
+chisq_test<-chisq.test(data_analysis)
+
+#P value of the test can also be calculated by using Monte Carlo Simulation by using the 'simulate.p.value' argument
+
+#Chisquare statistic
+
+chisq_test$p.value
+
+#Chisquare statistic
+
+chisq_test$statistic
+
+
