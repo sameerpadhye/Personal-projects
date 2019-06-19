94b36292acaa327b8b37f03eb9547851b1b31d58
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-06-20 02:05:31 +0530

    added model evaluation code

--- data_analytic_utilities/inferential_stats_utils/Random_forests_regression.R
+++ data_analytic_utilities/inferential_stats_utils/Random_forests_regression.R
@@ -67,3 +67,98 @@ ggplot(data_analysis,
            y = var_1)) + 
     geom_point() + 
     geom_abline()
+
+
+###Model_evaluation in Random forest (Here code provided uses different variables than those given above)#
+
+
+#Data file path 
+
+data_path2<-paste0(getwd(),"/regression_data2.xlsx")
+
+
+#Importing data for analysis
+
+data_analysis2<-read_excel(data_path2,
+                           sheet=1)%>%
+    dplyr::select(Trait_1:Trait_4)
+
+
+# Creating indices to sample training and test datasets
+
+set.seed(1)
+
+sample_assignment <- sample(1:2, size = nrow(data_analysis2), prob = c(0.7,0.3), replace = TRUE)
+
+
+# Create a train, validation and tests from the original data using the indices
+
+train_data <- data_analysis2[sample_assignment == 1, ]    #training data subset using the sample_assignment index
+
+test_data <- data_analysis2[sample_assignment == 2, ]   #testing data subset using the sample_assignment index
+
+
+# Using the package rpart (for random forest)
+
+if(!require(rpart))install.packages('rpart')
+
+
+# Training the model using rpart 
+
+train_model <- rpart(formula = Trait_1 ~ ., 
+                     data = train_data, 
+                     method = "anova")
+
+
+# Look at the model output  
+
+train_model
+
+
+# Using the rpart.plot package to visualize the tree
+
+if(!require(rpart.plot))install.packages('rpart.plot')
+
+# Plot the tree model
+
+rpart.plot(x = train_model, yesno = 2, type = 2, extra = 0)
+
+
+# Predictions using the test dataset
+
+predictions <- predict(object = train_model,   # model object 
+                       newdata = test_data)  # test dataset
+
+
+#Obtaining the RMSE (root mean square error) using the package Metrics
+
+if(!require(Metrics))install.packages('Metrics') 
+
+rmse(actual = test_data$Trait_1, 
+     predicted = predictions)
+
+
+# Pruning the tree based on Complexity Parameter (CP) and visualizing the optimized tree
+
+# Plotting the CP Table
+
+plotcp(train_model)
+
+
+# Obtaining the least CP value based on cross-validated error
+
+least_cp_val_index <- which.min(train_model$cptable[, "xerror"])
+
+least_cp <- train_model$cptable[least_cp_val_index, "CP"]
+
+
+# Pruning the model using the least CP value
+
+optimum_tree_model <- prune(tree = train_model, 
+                            cp = least_cp)
+
+
+# Visualizing the new optimized model
+
+rpart.plot(x =optimum_tree_model, yesno = 2, type = 2, extra = 0)
+
