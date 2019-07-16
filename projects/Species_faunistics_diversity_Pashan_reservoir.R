9b69cf001ab7bd8e85a5e2a3dd320ccf5d836363
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-07-17 03:13:06 +0530

    Species richness estimations and other visualizations of faunal summary added

--- projects/Species_faunistics_diversity_Pashan_reservoir.R
+++ projects/Species_faunistics_diversity_Pashan_reservoir.R
@@ -78,40 +78,135 @@ pashan_long<-pashan_full_data%>%
 View(pashan_long)
 
 
-# Pashan data exploration and visualization 
+########## Pashan data exploration and visualization ###############
 
-#1. Family wise distribution of species in pre and post beautification periods
+#1. Total species in the pre and post beautification collections
 
-# Re-order the collection period groups for better plots
-
-pashan_long$collection_grp<-fct_relevel(pashan_long$collection_grp,"pre")
+## The pashan_long data contains multiple entries of the same species based on the samples collected. Hence, the data are modified to have uniqiue species identities with corresponding presence/absence (0/1) based on the collection groups
 
 #Obtaining the summarized data
 
-pashan_family_summ<-pashan_long%>%
-    mutate_if(is.character,
-              as.factor)%>%
-    group_by(Family,collection_grp)%>%
-    summarize(total_sp=sum(values))
-
-#Re-order the families based on total species number
-
-pashan_family_summ$Family<-fct_reorder(pashan_family_summ$Family,
-                                       pashan_family_summ$total_sp)
+pashan_sp_data<-pashan_long%>%
+    group_by(Family,
+             Species,
+             collection_grp)%>%
+    summarise(species_tot=sum(values))%>%
+    mutate(sp_pre_abs=ifelse(species_tot>0,1,0))
 
 
-#plot
+#plot 
 
-pashan_family_summ%>%
-    ggplot(aes(x=Family,
-               y=total_sp))+
-    geom_col(fill='orange')+
-    facet_wrap(~collection_grp)+
+pashan_sp_data%>%
+    group_by(collection_grp)%>%
+    summarise(sp_tot=sum(sp_pre_abs))%>%
+    ggplot(aes(x=collection_grp,
+               y=sp_tot))+
+    geom_col(fill='steelblue')+
     theme_bw(base_size = 15)+
     theme(axis.text.x = element_text(angle = 90, 
                                      hjust = 1))+
+    labs(x="Collection period",
+         y="Total species",
+         title = "Total species observed during two periods of collection")
+
+
+#2. Family wise distribution of species in pre and post beautification periods
+
+pashan_sp_data%>%
+    ggplot(aes(x=Family,
+               y=sp_pre_abs))+
+    geom_col(fill='orange')+
+    facet_wrap(~collection_grp)+
+    theme_bw(base_size = 15)+
     labs(x="Familes",
          y="Total species",
-         title = "Total species observed in Pashan")
+         title = "Total species observed in Pashan")+
+    coord_flip()
+
+
+#3. Individual species occurrences (counts) in the pre and post collections 
+
+pashan_species<-pashan_long%>%
+    mutate_if(is.character,
+              as.factor)%>%
+    group_by(Family,
+             Species,
+             collection_grp)%>%
+    summarize(total_occ=sum(values))%>%
+    arrange(desc(total_occ))
+
+#Re-order the species based on total occurrences
+
+pashan_species$Species<-fct_reorder(pashan_species$Species,
+                                    pashan_species$total_occ)
+
+#plot
+
+pashan_species%>%
+    ggplot(aes(x=Species,
+               y=total_occ))+
+    geom_col(fill='orange')+
+    facet_wrap(~collection_grp)+
+    theme_bw(base_size = 15)+
+    labs(x="Familes",
+         y="Occurrences",
+         title = "Species observed in Pashan")+
+    coord_flip()
+
+
+#Species richness estimates and other related summaries
+
+#The data required for the analysis need to be in wide format and hence the original data that was imported will be used. This has been done for the pre and post beautification period samples respectively
+
+#1. Pre beautification species richness estimates
+
+names(pashan_pre_2016)
+
+
+#Transposing the data for further calculations
+
+pashan_pre_2016t<-pashan_pre_2016 %>%
+    select_if(is.numeric)%>%
+    t(.)%>%
+    data.frame(.)
+
+
+#Obtaining the total number of species per sample
+
+sp_number_pre<-vegan::specnumber(pashan_pre_2016t)%>%
+    data.frame(.)%>%
+    rownames_to_column()%>%
+    dplyr::rename('sp_no'='.',
+                  'collection_years'='rowname')
+
+
+#scatter plot showing the species numbers in respective years considered as pre beautification
+
+sp_number_pre%>%
+    ggplot(aes(x=collection_years,
+               y=sp_no))+
+    geom_point(col='forestgreen',
+               size=4)+
+    theme_bw(base_size = 15)+
+    theme(axis.text.x = element_text(angle = 90, 
+                                     hjust = 1))+
+    labs(x="Collection period",
+         y="Total species",
+         title = "Total species observed during two periods of collection")
+
+
+#Generating species accumulation curves (using vegan).Chao2 is used here since the data are presence/absence data
+
+species_acc_pre<-specaccum(pashan_pre_2016t,
+                           method = "random",
+                           permutations = 999,
+                           conditioned = TRUE,
+                           gamma = "chao2")
+
+#View the result
+
+species_acc_pre
+
+
 
 #WIP
