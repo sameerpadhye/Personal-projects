ad1bd9d1803deef07da2c1052f16c8eb7b8c575b
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-08-20 15:44:08 +0530

    edited the code and added goodness of fit test

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/chisq_test.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/chisq_test.R
@@ -1,14 +1,23 @@
-#Chisquare test of Independence.
+#Chisquare test of Independence and Goodness of fit
+
+##Test of Independence
+
 #For information on the test, please refer to https://www.statisticssolutions.com/using-chi-square-statistic-in-research/
+
+#libraries used
+
+library(tidyverse)
+library(readxl)
+library(magrittr)
 
 #data file path.
 
-paste0(getwd(),'/data_analysis.csv')
+data_file_path<-"C:/Users/samee/Desktop/R data/sample_datasets/Chisquare_sample_data.xlsx"
 
 #Importing the data. 
 
 data_analysis<-read_excel(data_file_path,
-                         sheet=1)%>%
+                          sheet=1)%>%
     column_to_rownames(.,'Categories')
 
 #Exploring the dataset
@@ -23,12 +32,20 @@ table_data<-as.table(as.matrix(data_analysis))
 
 library(gplots)
 
-balloonplot(t(table_data), main ="Association strength", xlab ="", ylab="",
-            label = FALSE, show.margins = FALSE)
+balloonplot(t(table_data), 
+            main ="Association strength", 
+            xlab ="", 
+            ylab="",
+            label = FALSE, 
+            show.margins = FALSE)
+
 
 #Performing the Chisquare test. 
 
 chisq_test<-chisq.test(data_analysis)
+
+
+#results
 
 #P value of the test can also be calculated by using Monte Carlo Simulation by using the 'simulate.p.value' argument
 
@@ -48,4 +65,43 @@ if(!require(DescTools))install.packages('DescTools')
 DescTools::Phi(table_data)
 
 DescTools::CramerV(table_data)
+
+
+##Goodness of fit
+
+#Same data is used for convenience.
+
+
+#Obtaining the proportions (observed)
+
+goodness_data<-data_analysis%>%
+    rownames_to_column(.)%>%
+    dplyr::select(rowname,
+                  Phenomenon_1)%>%
+    column_to_rownames('rowname')%>%
+    mutate(freq=Phenomenon_1/sum(Phenomenon_1))%>%#obtaining the expected proportions
+    mutate(obs_values=data_analysis$Phenomenon_2) #using the Phenonmenon 2 values here as observed values 
+
+
+#Exploring the new data
+
+head(goodness_data,5)
+
+
+#Goodness of fit test
+
+goodness_chisq<-goodness_data%$% # using the %$% operator from magrittr 
+    chisq.test(x=obs_values,
+               p=freq)
+
+
+#Result
+
+#statistic
+
+goodness_chisq$statistic
+
+#p value
+
+goodness_chisq$p.value
 
