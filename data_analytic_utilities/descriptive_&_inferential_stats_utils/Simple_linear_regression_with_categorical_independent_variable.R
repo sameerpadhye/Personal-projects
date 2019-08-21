88c0a00e525b94b8f93ebace8d0dc6e8973f4fde
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-08-21 15:55:34 +0530

    added code for linear regression using continous variables

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/Simple_linear_regression_with_categorical_independent_variable.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/Simple_linear_regression_with_categorical_independent_variable.R
@@ -9,9 +9,9 @@ library(MASS)
 library(magrittr)
 
 
-#Data file path (here it is assumed that the sample data is saved in the working directory and saved as an excel file)
+#Data file path (here it is assumed that the sample data is saved in the working directory and saved as an excel file; data_for_analysis_ANOVA has been used here)
 
-data_path<-paste0(getwd(),"/multiple_regression_data.xlsx")
+data_path<-"C:/Users/samee/Desktop/R data/sample_datasets/data_for_analysis_ANOVA.xlsx"
 
 
 #Importing data for analysis
@@ -60,6 +60,76 @@ data_analysis$predictions <- predict(model_cat_reg)
 
 # Plot predictions vs actual values (predictions on x-axis)
 
-ggplot(data_analysis, aes(x = predictions, y = Trait_1)) + 
+ggplot(data_analysis, aes(x = predictions, 
+                          y = Trait_1)) + 
     geom_point() +
-    geom_abline(color = "blue")
+    geom_abline(color = "blue")+
+    theme_bw(base_size = 16)+
+    ggtitle("Scatterplot")
+
+
+#2. Continous data analysis
+
+# Same original data has been used 
+
+# Importing the data
+
+data_analysis2<-read_excel(data_path,
+                           sheet=1)%>%
+    dplyr::select(Trait_1,
+                  Trait_2)
+
+
+#Scatterplot to visualize the association of the two continous variables
+
+data_analysis2%>%
+    ggplot(aes(Trait_1,
+               Trait_2))+
+    geom_point(size=3,
+               col='forestgreen')+
+    theme_bw(base_size = 16)+
+    ggtitle("Scatterplots")
+
+
+#Correlation of the two variables (to check linear association)
+
+data_analysis2%$%
+    cor.test(Trait_1,
+             Trait_2,
+             method = c("pearson"))
+
+
+# Regression formula
+
+formula_regression2<-as.formula("Trait_1~Trait_2")
+
+
+# Fitting the model
+
+model_cont_reg <-  lm(formula_regression2, 
+                      data = data_analysis2)
+
+
+# Summary of the model
+
+summary(model_cont_reg)
+
+
+#Adjusted R squared value (strength of the regression model)
+
+summary(model_cont_reg)$adj.r.squared
+
+
+# Predicting the outcome variable
+
+data_analysis2$predictions <- predict(model_cont_reg)
+
+
+# Plot predictions vs actual values (predictions on x-axis)
+
+ggplot(data_analysis2, aes(x = predictions, 
+                           y = Trait_1)) + 
+    geom_point() +
+    geom_abline(color = "blue")+
+    theme_bw(base_size = 16)+
+    ggtitle("Scatterplot")
