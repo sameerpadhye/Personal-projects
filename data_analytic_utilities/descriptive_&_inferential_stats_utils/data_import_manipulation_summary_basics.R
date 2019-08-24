3661afb6ab18875bf3114d82324caee5ec71e52e
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-08-24 15:59:40 +0530

    added code for removing NA's, subsetting and column and rowise summation

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/data_import_manipulation_summary_basics.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/data_import_manipulation_summary_basics.R
@@ -1,6 +1,6 @@
 #Basics of data import, manipulation and summary of data used in diversity/ecology studies 
 
-# Data used here (as in many studies) are dataframe data structures
+# Data used here (as in many studies) are dataframe data structures. For basics on dataframes please visit these links
 
 
 #1. Data import
@@ -31,8 +31,8 @@ data_path_wd<-paste0(getwd(),"/data_for_visualizations.xlsx")
 #importing a csv data file. The first argument here should either be the 
 
 data_for_analysis_csv<-read.csv(data_path_csv,
-                            header=TRUE, # Header of columns
-                            stringsAsFactors=FALSE) #whether strings should be treated as factors or not
+                                header=TRUE, # Header of columns
+                                stringsAsFactors=FALSE) #whether strings should be treated as factors or not
 
 #There are other multiple arugments for read.csv function which can be checked by using ?read.csv
 
@@ -45,9 +45,9 @@ data_analysis_choose<-read.csv(file.choose())
 #Importing excel data for analysis using readxl package
 
 if(!require(readxl))install.packages('readxl') 
-
+library(readxl)
 data_analysis<-readxl::read_excel(data_path_xl,
-                         sheet=1) # the sheet to be imported
+                                  sheet=1) # the sheet to be imported
 
 #OR
 
@@ -105,3 +105,74 @@ sapply(data_analysis,
        anyNA)
 
 
+#Removing NA if/any from the columns
+
+#To check if there are any NA's (FALSE means NA is present at that row position)
+
+NA_index<-complete.cases(data_analysis)
+
+# selecting only the TRUE (non NA values)
+
+data_analysis[NA_index,]
+
+
+#Obtaining the column names 
+
+colnames(data_analysis)
+
+
+#Obtaining the row names 
+
+rownames(data_analysis)
+
+
+#Accessing specific rows and columns (subsetting)
+
+data_analysis[1,2] # accessing first row from second column
+
+data_analysis[c(1:3),]  # accessing the first three rows of all columns
+
+data_analysis[,c(1,2)] # acessing the first two columns 
+
+data_analysis[c("var_1","var_3")] # accessing the columns by name (instead of position)
+
+#Using subset function
+
+subset(data_analysis,  # the dataframe
+       Factor=="Factor_A",  # subsetting by the category (Factor_A here)
+       select=var_1:var_3) # subsetting only var_1 to var_3
+
+
+#Obtaining rowsums of the dataframe (Only the numeric data has to be selected for this function)
+
+rowSums(data_analysis[,c(2:4)])
+
+#using apply for rowSums
+
+apply(data_analysis[,c(2:4)],1,sum) # The number '1' signifies rowwise calculations while 'sum' states the function to be used (which is sum of values) here
+
+
+#Obtaining colsums of the dataframe (Only the numeric data has to be selected for this function)
+
+colSums(data_analysis[,c(2:4)])
+
+#using apply for rowSums
+
+apply(data_analysis[,c(2:4)],2,sum) # The number '2' signifies columnwise calculations while 'sum' states the function to be used (which is sum of values) here
+
+
+# Viewing the levels of a factor data type. First it should be checked whether Factor data type is present in the dataframe (by str function)
+
+#a. checking the structure of the data
+
+str(data_analysis) 
+
+# Since the Factor column is character, it is first converted to a factor
+
+#b. Converting character (one data type) to factor (another data type) data type
+
+data_analysis$Factor<-as.factor(data_analysis$Factor)
+
+#c. Viewing the levels of the factor
+
+levels(data_analysis$Factor)
