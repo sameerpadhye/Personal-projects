f0bf404f5d4b7b7fd632fb0b7056e188055b8221
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-08-26 15:58:03 +0530

    code for data summarization added

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/data_import_manipulation_summary_basics.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/data_import_manipulation_summary_basics.R
@@ -143,7 +143,9 @@ subset(data_analysis,  # the dataframe
        select=var_1:var_3) # subsetting only var_1 to var_3
 
 
-#Obtaining rowsums of the dataframe (Only the numeric data has to be selected for this function)
+#Obtaining data summary of rows of the dataframe (Only the numeric data has to be selected for this function)
+
+#a. sum
 
 rowSums(data_analysis[,c(2:4)])
 
@@ -151,14 +153,27 @@ rowSums(data_analysis[,c(2:4)])
 
 apply(data_analysis[,c(2:4)],1,sum) # The number '1' signifies rowwise calculations while 'sum' states the function to be used (which is sum of values) here
 
+#b. mean
 
-#Obtaining colsums of the dataframe (Only the numeric data has to be selected for this function)
+rowMeans(data_analysis[,c(2:4)])
+
+#using apply (the syntax remains same as for sum with the substitution of 'mean' instead of 'sum')
+
+#Obtaining data summary of columns of the dataframe (Only the numeric data has to be selected for this function)
+
+#a. sum
 
 colSums(data_analysis[,c(2:4)])
 
 #using apply for rowSums
 
 apply(data_analysis[,c(2:4)],2,sum) # The number '2' signifies columnwise calculations while 'sum' states the function to be used (which is sum of values) here
+
+#b. mean
+
+colMeans(data_analysis[,c(2:4)])
+
+#using apply (the syntax remains same as for sum with the substitution of 'mean' instead of 'sum')
 
 
 # Viewing the levels of a factor data type. First it should be checked whether Factor data type is present in the dataframe (by str function)
@@ -176,3 +191,104 @@ data_analysis$Factor<-as.factor(data_analysis$Factor)
 #c. Viewing the levels of the factor
 
 levels(data_analysis$Factor)
+
+
+#Binding rows and columns 
+
+#1. Columns
+
+#Additional rows and columns can be bound to the existing dataframe 
+
+#the current data is first subsetted into 2 dataframes
+
+data_1<-subset(data_analysis,
+               select = Factor:var_2)
+
+data_2<-subset(data_analysis,
+               select = var_3:var_4)
+
+#Column bind
+
+data_combined<-cbind(data_1,
+                     data_2)
+
+View(data_combined)
+
+
+#2. Rows
+
+#For binding rows the column names of both the datasets must be matching.
+#Hence if the same data used for column bind is used, an error will be obtained
+
+rbind(data_1,data_2)
+
+#Obtaining datasets for rbind
+
+data_3<-subset(data_analysis,
+               Factor=='Factor_A',
+               select = Factor:var_2)
+
+data_4<-subset(data_analysis,
+               Factor=='Factor_B',
+               select = Factor:var_2)
+
+#Row bind with the above datasets
+
+data_combined2<-rbind(data_3,
+                      data_4)
+
+View(data_combined2)
+
+# Attach a dataset for easier functionality
+
+attach(data_analysis)
+
+#after attaching the dataset, the name of the dataset need not be called upon everytime (which is data_analysis$)
+
+#getting basic summary of the data (after attaching the dataset)
+
+#mean
+mean(var_1,na.rm = T)
+
+#sum
+sum(var_2,na.rm = T)
+
+#Standard deviation
+sd(var_3,na.rm = T)
+
+#range
+range(var_4)
+
+#median
+median(var_1,na.rm = T)
+
+#Variance
+var(var_2)
+
+# the dataset can be detached after the analysis has been conducted
+
+detach(data_analysis)
+
+
+# Obtaining the complete data summary of the dataframe
+
+summary(data_analysis)
+
+
+#Obtaining the data summary of the numerical variables of the dataframe using 'describe' function from psych package
+
+if(!require(psych))install.packages('psych') 
+
+describe(subset(data_analysis,
+                select = var_1:var_4)) # here subset function is used to select the numerical variables
+
+
+#Data summary can also be obtained per group/s (Factors for this dataset) using describeBy function from psych
+
+describeBy(subset(data_analysis,
+                  select = var_1:var_4), # numerical data
+           group = data_analysis$Factor, # the grouping variable
+           mat = TRUE, # output returned as a matrix
+           digits = 3)  # rounding the digits in the output
+
+# here the attach and detach functions would be helpful 
