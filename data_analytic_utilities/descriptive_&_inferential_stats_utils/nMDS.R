784f55be4b50c55fcf426563aec39d355cfe6f32
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-09-25 15:32:09 +0530

    code for nMDS visualization by ggplot2 added

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/nMDS.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/nMDS.R
@@ -87,5 +87,49 @@ ordihull(data_nmds,
          label=F)
 
 
+# Visualization using ggplot2 (adapted from https://chrischizinski.github.io/rstats/vegan-ggplot2/)
+
+#Species scores 
+
+sp_scores<-vegan::scores(data_nmds,"species")%>%
+    data.frame(.)%>%
+    rownames_to_column(.)%>%
+    dplyr::rename("species"="rowname")
+
+#View result
+
+View(sp_scores)
+
+#Site scores
+
+site_scores<-vegan::scores(data_nmds)%>%
+    data.frame(.)%>%
+    rownames_to_column(.)%>%
+    dplyr::rename("sites"="rowname")%>%
+    dplyr::mutate(site_groups=data_analysis$Groups)
+
+# View result
+
+View(site_scores)
+
+
+# Plot
+
+ggplot() + 
+    geom_point(data=site_scores,aes(x=NMDS1,
+                                    y=NMDS2,
+                                    shape=site_groups,
+                                    colour=site_groups),
+               size=5) + 
+    geom_text(data=sp_scores,
+              aes(x=NMDS1,
+                  y=NMDS2,
+                  label=species),
+              vjust=0.7,
+              hjust=0.5) + 
+    scale_colour_manual(values=c("A" = "orange", "B" = "steelblue", "C" = "forestgreen")) +
+    coord_equal() +
+    theme_bw(base_size = 18)+
+    ggtitle("nMDS plot")
 
 
