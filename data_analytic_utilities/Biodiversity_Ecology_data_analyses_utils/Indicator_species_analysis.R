ae42d6add0df0ddc92fddd6f315f18689f5af662
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-10-02 15:37:13 +0530

    code for indicator species calculation added

--- data_analytic_utilities/Biodiversity_Ecology_data_analyses_utils/Indicator_species_analysis.R
+++ data_analytic_utilities/Biodiversity_Ecology_data_analyses_utils/Indicator_species_analysis.R
@@ -66,5 +66,24 @@ summary(species_associations)
 
 #B) Indicator species 
 
+
 # The same indicspecies package is used to find out the indicator species for the specified groups of localities using the 'indicator' function
-# WIP
+
+
+species_indicators<-indicspecies::indicators(subset(data_analysis,
+                                                    select = Species_1:Species_10),# the species community dataset (sites X species)
+                                             cluster=data_analysis$Groups, 
+                                             group = 'A', # The group for which the indicator species combination is sought
+                                             func = "IndVal.g", # how the index will be calculated (based on the species data and number of sites in each group)
+                                             alpha=0.01, # the cutoff p value
+                                             nboot = 499) # bootstrap number for confidence interval calculation
+
+
+# Obtaining the results 
+
+
+print(species_indicators)
+
+
+#For more information on the additional arguments used in the functions and a general explaination of the results, please refer to 
+# https://cran.r-project.org/web/packages/indicspecies/indicspecies.pdf
