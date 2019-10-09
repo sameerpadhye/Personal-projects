f6545d0bbb402e9183248a57b224b86cd1703804
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-10-09 15:24:04 +0530

    edited the code and added code for new posthoc tests

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/Statistical_Inferential_tests/Kruskal_Wallis_test.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/Statistical_Inferential_tests/Kruskal_Wallis_test.R
@@ -1,15 +1,19 @@
 # Kruskal Wallis test 
 
 
+#libraries used
+
+library(tidyverse)
+
 ## data used for analysis
 
 
 data_analysis<-data.frame(Habitats=rep(c("Habitat_1",
-                                   "Habitat_2",
-                                   "Habitat_3",
-                                "Habitat_4"),
-                                 each=20),
-    species_data=sample(c(100:500),80,replace = TRUE))
+                                         "Habitat_2",
+                                         "Habitat_3",
+                                         "Habitat_4"),
+                                       each=20),
+                          species_data=sample(c(100:500),80,replace = TRUE))
 
 
 # View the data
@@ -23,10 +27,15 @@ head(data_analysis,5)
 
 data_analysis%>%
     ggplot(aes(x=Habitats,
-               y=species_data))+
-    geom_boxplot(fill='steelblue')+
+               y=species_data,
+               fill=Habitats))+
+    geom_boxplot()+
     theme_bw(base_size = 18)+
-    ggtitle("Boxplot of species data")
+    ggtitle("Boxplot of species data")+
+    scale_fill_manual(values=c("#00AFBB", 
+                               "#E7B800", 
+                               "#FC4E07",
+                               "forestgreen"))
 
 
 # Checking the normality of the data (between the four habitats). For convenience, data is attached for the test and then detached
@@ -51,9 +60,42 @@ kruskal.test(species_data ~ Habitats,
 # Post hoc tests using the package FSA
 
 
+#Using FSA package (Dunn test)
+
+
 if(!require(FSA))install.packages('FSA') 
 
 
 dunnTest(species_data ~ Habitats, 
          data = data_analysis,
          method="bh")
+
+
+#Using DescTools package (Nemenyi Test) (should not be used if observations per groups is unequal) 
+
+
+if(!require(DescTools))install.packages('DescTools') 
+
+
+attach(data_analysis)
+
+
+NemenyiTest(species_data,
+            Habitats, 
+            dist='tukey')
+
+
+detach(data_analysis)
+
+
+#Using pairwise wilcoxon tests
+
+
+attach(data_analysis)
+
+
+pairwise.wilcox.test(species_data,
+                     Habitats,
+                     p.adjust.method="none")
+
+detach(data_analysis)
