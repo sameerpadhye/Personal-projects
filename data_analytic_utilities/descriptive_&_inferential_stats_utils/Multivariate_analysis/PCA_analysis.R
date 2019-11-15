402d0d93bf274b90fac4389447a9f8c78cae1a7d
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-11-15 11:50:44 +0530

    added code for calculating individual descriptor contribution to the PCA axes

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/Multivariate_analysis/PCA_analysis.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/Multivariate_analysis/PCA_analysis.R
@@ -3,39 +3,61 @@
 
 
 #Libraries used
+
+
 library(tidyverse)
 library(magrittr)
 library(readxl)
 
 
-#Path for the data file
-data_path<-"C:/Users/samee/Desktop/R data/PCA_data_git.xlsx"
+# Data path
+
+
+data_path<-"C:/Users/samee/Desktop/Personal-projects/sample_datasets/PCA_data_git.xlsx"
+
 
 #Importing data for analysis
+
+
 PCA_data<-read_excel(data_path,
                           sheet=1)%>%
     mutate_if(is.character,
               as.factor)
 
-#exploring the structure of the data
+
+# Exploring the structure of the data
+
+
 str(PCA_data)
 
+
 # PCA analysis
+
+
 pca_analysis<- PCA_data%>%
     dplyr::select_if(is.numeric)%>% # only numeric data should be selected
     data.frame(.)%>% # converted to dataframe since tibble is returned
     prcomp(.,scale. = T) # scale is TRUE when column descriptors are of different scales (E.g. one column contains pH values while the second contains Temperature)
 
+
 #extracting the PCA co-ordinates
+
+
 pca_analysis_values<-pca_analysis$x
 
-#extracting PCA vector values of the first two axes (the first two vectors usually explain the maximum variation)
 
-pca_analysis_vector<-pca_analysis$rotation[,c(1,2)]
+#extracting PCA vector values 
+
+
+pca_analysis_vector<-pca_analysis$rotation
+
 
 #plot for PCA
 
+
 #1.autoplot function from the package ggplot2 used to visualize the result s of the PCA analysis
+
+
 ggplot2::autoplot(pca_analysis, 
          scale = 0, 
          data = PCA_data, 
@@ -53,8 +75,70 @@ ggplot2::autoplot(pca_analysis,
          loadings.label.hjust = 0.5, 
          loadings.label.vjust = 1.2)
 
-## There are mulitple arguments not passed here that can be added as per requirement.    
+
+## There are mulitple arguments not passed here that can be added as per requirement (ggplot arguments can also passed to the above plot)
 
 ##Scree plot for PCA (used to visualize the contribution of each eigenvector in explaining the variation)
+
+
 library(factoextra)
+
+
 fviz_eig(pca_c.hislopi)
+
+
+#Calculating the contribution of individual descriptors to the PCA axes
+
+
+# (Adopted from: http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/)
+
+
+#1. Obtaining the Coordinates
+
+
+PCA_loadings <- pca_analysis$rotation
+
+
+std.dev <- pca_analysis$sdev
+
+
+coord_env_var <- t(apply(PCA_loadings, 
+                         1, function (x) {x*std.dev}))
+
+
+head(coord_env_var,5)
+
+
+#2. Obtaining the cos2 values
+
+
+coord_env_var_cos2 <- coord_env_var^2
+
+
+head(coord_env_var_cos2)
+
+
+#3 Obtaining the contributions 
+
+
+#a.
+
+
+PCA_env_var_contri <- apply(coord_env_var_cos2, 2, sum)
+
+
+#b.
+
+
+env_contri_fn <- function(cos2_val, cos2_add){cos2_val*100/cos2_add}
+
+
+env_var_contri <- t(apply(coord_env_var_cos2,
+                          1, 
+                          env_contri_fn,
+                          PCA_env_var_contri))
+
+
+head(env_var_contri,5)
+
+
