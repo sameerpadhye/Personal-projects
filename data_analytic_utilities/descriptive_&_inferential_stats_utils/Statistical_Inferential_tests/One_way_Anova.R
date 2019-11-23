42d4863c4f280ec7724f544dbff8ff2c2151cfa1
Sameer Padhye <sameer.m.padhye@gmail.com>
2019-11-23 16:49:18 +0530

    edited the code, added plotly plot and code for checking the distribution of residuals

--- data_analytic_utilities/descriptive_&_inferential_stats_utils/Statistical_Inferential_tests/One_way_Anova.R
+++ data_analytic_utilities/descriptive_&_inferential_stats_utils/Statistical_Inferential_tests/One_way_Anova.R
@@ -13,16 +13,20 @@ library(readxl)
 
 data_file<-paste0(getwd(),"/data_for_analysis_ANOVA.xlsx")
 
+
 #Data for analysis. Data should be in long format (stacked data)
+
 data_for_analysis<-read_excel(data_file,
                               sheet=2)%>%
     mutate_if(is.character,
               as.factor)
 
 #Viewing the data
+
 head(data_for_analysis,3)
 
 # Viewing the levels of the Factor
+
 levels(data_for_analysis$Factor)
 
 #Summarizing the data
@@ -35,14 +39,15 @@ data_for_analysis%>%
                            na.rm = T))
 
 #Data visualization
+
 data_for_analysis%>%
-    ggplot(aes(x=Factor,
-               y=Trait))+
-    geom_boxplot(col='black',
-                 fill="orange")+
-    theme_bw(base_size = 18)+
-    ylab("Trait_values")+
-    xlab("Factor")
+    plot_ly(x = ~Factor, 
+            y = ~Trait,
+            color=~Factor) %>%
+    add_boxplot(opacity=0.6)%>%
+    layout(title = 'Boxplot',
+           axis = list(title = "Factors"),
+           yaxis = list(title = "Trait"))
 
 #Checking the data for normality 
 
@@ -55,13 +60,35 @@ data_for_analysis%>%
 
 #ANOVA
 
+#1. using AOV
+
 data_for_analysis%$%
     aov(Trait~Factor,
         data=.)%>%
     summary(.)
 
+#2. using lm
+
+lm_aov_model<-data_for_analysis%$% #this type of pipe is used from piper library 
+    lm(Trait~Factor,
+       data=.)
+
+summary(lm_aov_model)
+
+# Checking the residuals distribution 
+
+# Homoskedasticity
+
+plot(lm_aov_model) 
+
+# residual normality
+
+hist(residuals(lm_aov_model)) 
+
 #Tukey Posthoc test
+
 data_for_analysis%$%
     aov(Trait~Factor,
         data=.)%>%
     TukeyHSD(.)
+
