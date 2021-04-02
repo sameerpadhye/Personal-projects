8632c2d3ce63e119c3deea79454fa6032cf0fa17
Sameer Padhye <sameer.m.padhye@gmail.com>
2021-04-02 16:38:35 +0530

    added new code for analysis

--- ongoing_studies/obtaining species keywords from pdfs using fuzzy matching.R
+++ ongoing_studies/obtaining species keywords from pdfs using fuzzy matching.R
@@ -8,6 +8,8 @@ library(reshape2)
 library(tidyverse)
 library(tidytext)
 library(magrittr)
+library(fuzzyjoin)
+
 
 #1. Import pdf
 
@@ -34,43 +36,39 @@ text_path<-choose.files()
 
 # using tm for corpus
 
-corpus_data_path<-"C:/Users/samee/Desktop/R data/sample_datasets/sample_pdfs/Diversity and zoogeography of fairy shrimps on the Indian subcontinent.pdf"
-
-library(tm)
-
-corpus_txt<-Corpus(URISource(corpus_data_path),
-       readerControl = list(reader = readPDF))%>%
-    tm_map(., removePunctuation, ucp = TRUE)
-
-corpus_txt
-
-text_tdm<-TermDocumentMatrix(corpus_txt, 
-                             control = 
-                                 list(removePunctuation = TRUE,
-                                      stopwords = TRUE,
-                                      removeNumbers = TRUE))
-
-inspect(text_tdm)
-
-matrix_text<-as.matrix(text_tdm)%>%
-    data.frame(.)%>%
-    rownames_to_column(.)%>%
-    dplyr::rename("species_name"="rowname")
-
-View(matrix_text)
-
-approx_names<-data.frame(species_name = c("dichotomus", "hardingi", "orientalis",
-                                "spinifer", "simplex", "maduraensis"))
-
-
-join_trial_1<-matrix_text%>%
-    stringdist_left_join(approx_names, 
-                         method = "soundex", 
-                         by="species_name")%>%
-    drop_na(.)
-
-
-View(join_trial_1)
+# corpus_data_path<-"C:/Users/samee/Desktop/R data/sample_datasets/sample_pdfs/Diversity and zoogeography of fairy shrimps on the Indian subcontinent.pdf"
+# 
+# corpus_txt<-Corpus(URISource(corpus_data_path),
+#        readerControl = list(reader = readPDF))%>%
+#     tm_map(., removePunctuation, ucp = TRUE)
+# 
+# corpus_txt
+# 
+# text_tdm<-TermDocumentMatrix(corpus_txt, 
+#                              control = 
+#                                  list(removePunctuation = TRUE,
+#                                       stopwords = TRUE,
+#                                       removeNumbers = TRUE))
+# 
+# inspect(text_tdm)
+# 
+# matrix_text<-as.matrix(text_tdm)%>%
+#     data.frame(.)%>%
+#     rownames_to_column(.)%>%
+#     dplyr::rename("species_name"="rowname")
+# 
+# View(matrix_text)
+# 
+# approx_names<-data.frame(species_name = c("dichotomus", "hardingi", "orientalis",
+#                                 "spinifer", "simplex", "maduraensis"))
+# 
+# join_trial_1<-matrix_text%>%
+#     stringdist_left_join(approx_names, 
+#                          method = "soundex", 
+#                          by="species_name")%>%
+#     drop_na(.)
+# 
+# View(join_trial_1)
 
 ## For converting corpus to plain text
 
@@ -98,22 +96,94 @@ data_path<-"C:/Users/samee/Downloads/trial_pdfs_text_data_shalini"
 pdf_files<-list.files(data_path,pattern = '*.pdf')%>%
     paste0(data_path,"/",.)
 
-pdf_files
+pdf_files[1]
+
+# list of reference words
+
+list_of_words<-data.frame(word_list=c("Pandemic", "Urban", "Foraging", "Wild Plants","Lockdown", "Gathering", "Covid-19", "Wild plants", "Food", "Green spaces", "wild edibles", "isolation", "alternative food"))
 
 crpus_to_tokens<-function(x){
     
-    x%>%
+   step_1= x%>%
         pdftools::pdf_text(.)%>%
         quanteda::corpus(.)%>%
         quanteda::tokens(.,
                          remove_punct = TRUE, 
-                         remove_numbers = TRUE)
+                         remove_numbers = TRUE)%>%
+        dfm(.)%>%
+        t(.)%>%
+        data.frame(.)%>%
+        dplyr::rename("word_list"="doc_id")
+    
+   step_2 = step_1%>%
+       rowwise()%>%
+       dplyr::mutate(word_counts = sum(across(starts_with('text'))))%>%
+       select(word_list,word_counts)%>%
+       stringdist_inner_join(list_of_words, 
+                             method = "soundex", 
+                             by="word_list")
+      return(step_2)
     # try to run the entire process within the function and use map to get the results for each pdf
 }
 
 
-token_text<-purrr::map2_df(pdf_files,crpus_to_tokens)
+token_text<-purrr::map(pdf_files,crpus_to_tokens)
 
-class(pdf_files)
+names(token_text)<-sprintf("text%d",seq=(1:length(token_text)))
 
-list_of_words<-data.frame(word_list=c("Pandemic", "Urban", "Foraging", "Wild Plants","Lockdown", "Gathering", "Covid-19", "Wild plants", "Food", "Green spaces", "wild edibles", "isolation", "alternative food"))
+token_text['text1']
+
+View(token_text[[3]])
+
+token_text[[3]]%>%
+    mutate(sample=sprintf("text%d",rep=(times=nrow(.))))
+
+library(plyr)
+
+ldply(token_text, 
+      data.frame)
+
+token_text[[3]]%>%
+    dplyr::select(word_list.x,word_counts)
+    
+
+
+# trial_1_1<-trial_1%>%
+#     stringdist_inner_join(list_of_words, 
+#                          method = "soundex", 
+#                          by="word_list")%>%
+#     column_to_rownames(.,'word_list.x')%>%
+#     dplyr::select_if(is.numeric)%>%
+#     rowSums(.)%>%
+#     data.frame(.)%>%
+#     rownames_to_column(.)%>%
+#     rename("counts"='.',
+#            "words"="rowname")%>%
+#     arrange(counts)
+#     
+# 
+# View(trial_1_1)
+# 
+# trial_2<-token_text[[2]]%>%
+#     dfm(.)%>%
+#     t(.)%>%
+#     data.frame(.)%>%
+#     dplyr::rename("word_list"="doc_id")
+# 
+# View(trial_1_1)
+# 
+# 
+# trial_2_1<-trial_2%>%
+#     stringdist_inner_join(list_of_words, 
+#                           method = "soundex", 
+#                           by="word_list")%>%
+#     column_to_rownames(.,'word_list.x')%>%
+#     dplyr::select_if(is.numeric)%>%
+#     rowSums(.)%>%
+#     data.frame(.)%>%
+#     rownames_to_column(.)%>%
+#     rename("counts"='.',
+#            "words"="rowname")%>%
+#     arrange(counts)
+# 
+# View(trial_2_1)
